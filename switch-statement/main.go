6baa45ee4a4d5ccc86c75caf03c65fc27e0e7dd7
JobinAj <jobinaj6361@gmail.com>
2025-11-05 07:35:17 +0530

    learnt switch cases

---
+++ switch-statement/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var x int
+	println("enter the number:")
+	fmt.Scanln(&x)
+
+	switch x {
+	case 43:
+		fmt.Println("x is 43")
+	case 44:
+		fmt.Println("x is 44")
+	case 45:
+		fmt.Println("x is 45")
+	case 40:
+		fmt.Println("x is 40")
+	default:
+		fmt.Println("doesn't matches any cases")
+	}
+}
