de59d46f5f8c1613b002a6e67a73a4c05d25dbeb
JobinAj <jobinaj6361@gmail.com>
2025-11-12 07:26:03 +0530

    feat:added more print statements for printing random numbers

--- excercise-19/main.go
+++ excercise-19/main.go
@@ -10,13 +10,23 @@ func main() {
 	fmt.Printf("The random Value generated is:%v\n", x)
 
 	if x <= 100 {
-		fmt.Printf("the value is less than 100\nTHE VALUE:%v", x)
+		fmt.Printf("the value is less than 100\nTHE VALUE:%v\n", x)
 	} else if x > 100 && x <= 200 {
-		fmt.Printf("the value between 100&200 is\nTHE VALUE:%v", x)
+		fmt.Printf("the value between 100&200 is\nTHE VALUE:%v\n", x)
 	} else if x > 200 && x <= 250 {
-		fmt.Printf("the value between 200&250 is\nTHE VALUE:%v", x)
+		fmt.Printf("the value between 200&250 is\nTHE VALUE:%v\n", x)
 	} else {
-		fmt.Printf("this was more than 250\nTHE VALUE IS:%v", x)
+		fmt.Printf("this was more than 250\nTHE VALUE IS:%v\n", x)
 	}
+	fmt.Println(rand.Intn(4))
+	fmt.Println(rand.Intn(4))
+	fmt.Println(rand.Intn(4))
+	fmt.Println(rand.Intn(4))
+	fmt.Println(rand.Intn(4))
+	fmt.Println(rand.Intn(4))
+	fmt.Println(rand.Intn(4))
+	fmt.Println(rand.Intn(4))
+	fmt.Println(rand.Intn(4))
+	fmt.Println(rand.Intn(4))
 
 }
