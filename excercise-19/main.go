0182e5b21d10939a1cc267d8ecbec2725098b829
JobinAj <jobinaj6361@gmail.com>
2025-11-12 07:14:13 +0530

    excercise19

---
+++ excercise-19/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	x := rand.Intn(300)
+	fmt.Printf("The random Value generated is:%v\n", x)
+
+	if x <= 100 {
+		fmt.Printf("the value is less than 100\nTHE VALUE:%v", x)
+	} else if x > 100 && x <= 200 {
+		fmt.Printf("the value between 100&200 is\nTHE VALUE:%v", x)
+	} else if x > 200 && x <= 250 {
+		fmt.Printf("the value between 200&250 is\nTHE VALUE:%v", x)
+	} else {
+		fmt.Printf("this was more than 250\nTHE VALUE IS:%v", x)
+	}
+
+}
