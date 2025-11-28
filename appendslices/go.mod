876c45cfec75900592f63a68770f272c9bf08051
JobinAj <jobinaj6361@gmail.com>
2025-11-28 06:26:59 +0530

    learnt:how to append slice

---
+++ appendslices/go.mod
@@ -0,0 +1,3 @@
+module appendslice
+
+go 1.25.0
---
+++ appendslices/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	xs := []string{"jobin"}
+	xs = append(xs, "aj")
+	fmt.Println("Final slice:", xs)
+
+}
