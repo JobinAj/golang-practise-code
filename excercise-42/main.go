feedbb78004bcda14ffc77de82e66eea10b02702
jobinaj <jobinaj6361@gmail.com>
2025-12-09 07:07:53 +0530

    modified  code and added for loop to assign value to the index of the array

--- excercise-42/main.go
+++ excercise-42/main.go
@@ -1,13 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	xi := [5]int{10, 11, 12, 13, 14}
+	/*
+		xi := [5]int{10, 11, 12, 13, 14}
 
+		for i, v := range xi {
+
+			fmt.Printf("the index is %v\t the value is %v\n", i, v)
+		}
+	*/
+	xi := [5]int{}
+
+	for i := 0; i < 5; i++ {
+		xi[i] = i
+
+	}
 	for i, v := range xi {
-
-		fmt.Printf("the index is %v\t the value is %v\n", i, v)
+		fmt.Printf("the value is %v\t\n the index is %v\t\n the type is %T\n", i, v, v)
 	}
 
 }
