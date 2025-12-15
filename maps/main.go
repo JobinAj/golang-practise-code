bdae0587a95efbc2ed2c3c3f76e8365984d6b050
jobinaj <jobinaj6361@gmail.com>
2025-12-15 07:34:36 +0530

    learnt: why do we use maps and why do we use make and when do we use map literals thanks to god

---
+++ maps/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	people := map[string]int{
+		"jobin aj": 22,
+		"alvin aj": 26,
+	}
+	fmt.Println("the people age is", people["jobin aj"])
+	fmt.Println("the length of the map is", len(people))
+	fmt.Println(people)
+
+	timemachinevalues := make(map[string]int)
+
+	timemachinevalues["i don't know the values actually that's why i'm using make😅"] = 10
+	timemachinevalues["i don't know the values actually that's why i'm using make i don't know second thing also😅😅"] = 20
+
+	fmt.Println(timemachinevalues["i don't know the values actually that's why i'm using make😅"])
+	fmt.Println(timemachinevalues["i don't know the values actually that's why i'm using make i don't know second thing also😅😅"])
+
+}
