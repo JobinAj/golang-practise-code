b163c0f83420693724d5d0127d3b3e6afb0c6172
jobinaj <jobinaj6361@gmail.com>
2025-12-31 07:10:24 +0530

    pratised: created maps using make and then added value of string of slice and then created a inner loop to print the index and the value of the slice of strings

---
+++ excercise-49/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := make(map[string][]string)
+	m["bond_james"] = []string{"shaken", "not stirred", "martinis", "fast cars"}
+	m["moneypenny_jenny"] = []string{"intelligence", "literature", "computer science"}
+	m["no_dr"] = []string{"cats", "ice cream", "sunsets"}
+
+	fmt.Printf("%#v\n", m)
+
+	for k, v := range m {
+		fmt.Println(k, v)
+		for i, v2 := range v { //this inner loop prints the index of each value the slice of strings
+			fmt.Println(i, v2)
+		}
+	}
+}
