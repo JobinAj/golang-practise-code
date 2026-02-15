f84035105cc6e39b802433c3df12ce516bf90581
jobinaj <jobinaj6361@gmail.com>
2026-02-15 17:46:01 +0530

    practised: how does named return works in the go programming

---
+++ exercises/exercise-31/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := sum([]int{1, 2, 3, 4, 5, 6, 7, 8, 9})
+	fmt.Println(x)
+
+}
+
+func sum(ii []int) int {
+	total := 0
+	for _, v := range ii {
+		total += v
+	}
+	return total //named return
+
+}
