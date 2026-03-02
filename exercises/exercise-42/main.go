6483702879d5cee4851fa3754552aaea5df72f5a
jobinaj <jobinaj6361@gmail.com>
2026-03-02 11:51:40 +0530

    practised:wrote a program for func expression meaning-assigned a variblae to the anonymous function and called that varible

---
+++ exercises/exercise-42/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	name := func() { //assigned a function to the variable and the function is a anonymous function
+		println("jobinaj")
+
+	}
+	name() //calling the varible
+}
