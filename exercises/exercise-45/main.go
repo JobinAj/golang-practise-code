a8cfc70d622c44b12dffd5bef555b252e48ff700
jobinaj <jobinaj6361@gmail.com>
2026-03-05 07:03:44 +0530

    practised: wrote a closure function program closure function basically is variable which exists even after the execution of it's function

--- exercises/exercise-45/main.go
+++ exercises/exercise-45/main.go
@@ -18,6 +18,6 @@ func powernator(a float64) func() float64 {
 	var c float64
 	return func() float64 {
 		c++
-		return math.Pow(a, c)
+		return math.Pow(a, c) // this is the closure function because the outer funcion execution has finished and the variable is still being used and values are returned so this is a closure function.
 	}
 }
