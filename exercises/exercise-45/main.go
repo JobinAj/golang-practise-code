82f42ac9718f6e6f8bb5161e371ba4fa7ba40a0f
jobinaj <jobinaj6361@gmail.com>
2026-03-05 07:03:32 +0530

    practised: wrote a closure function program closure function basically is variable which exists even after the execution of it's function

---
+++ exercises/exercise-45/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	x := powernator(3)
+	fmt.Println(x())
+	fmt.Println(x())
+	fmt.Println(x())
+	fmt.Println(x())
+	fmt.Println(x())
+}
+
+func powernator(a float64) func() float64 {
+	var c float64
+	return func() float64 {
+		c++
+		return math.Pow(a, c)
+	}
+}
