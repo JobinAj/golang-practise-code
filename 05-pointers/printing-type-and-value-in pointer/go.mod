a6f74f41b104c57f29851b22bb1a0b6ef1a3ca8f
jobinaj <jobinaj6361@gmail.com>
2026-03-11 07:05:00 +0530

    learnt: wrote a simple program to demonstrate how does  printing a value and also printing it's type works in pointer

---
+++ 05-pointers/printing-type-and-value-in pointer/go.mod
@@ -0,0 +1,3 @@
+module type-and-value
+
+go 1.25.0
---
+++ 05-pointers/printing-type-and-value-in pointer/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := "jobinaj"
+	b := 5
+	fmt.Printf("The the type is %T and the value is %v\n", &a, &a)
+	fmt.Printf("The the type is %T and the value is %v\n", &b, &b)
+}
