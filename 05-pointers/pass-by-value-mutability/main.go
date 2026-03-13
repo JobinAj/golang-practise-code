b2d287254435eca32b7511cde2a02902d2b47297
jobinaj <jobinaj6361@gmail.com>
2026-03-13 06:47:14 +0530

    fix:Added comments

--- 05-pointers/pass-by-value-mutability/main.go
+++ 05-pointers/pass-by-value-mutability/main.go
@@ -15,7 +15,7 @@ func DeltaSlice(n []int) {
 }
 
 // map referance type
-func DeltaMap(md map[string]int, nextyear string) {
+func DeltaMap(md map[string]int, nextyear string) { //map need it's key to be passed in while modifiying the value.
 	md[nextyear] = 23
 }
 
@@ -35,7 +35,7 @@ func main() {
 	//passing value to the map referance type
 	m := make(map[string]int)
 	m["jobin"] = 22
-	fmt.Println(m["jobin"])
+	fmt.Println(m["jobin"]) //here we are passing the jobin because if not passed then it doesn't know which to change now here only one key is there but if there was more key and value in the map then it will not a best practise
 	DeltaMap(m, "jobin")
 	println(m["jobin"])
 }
